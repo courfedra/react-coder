5a8489ae9aaf6bb0cad9aa65321cc3da4a280a58
courfedra <franandres93@gmail.com>
2022-11-20 15:32:41 -0300

    probando App.js y app.css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
 const App = () => {
   return (
-    <h1 className="titulo">Hola Mundo!</h1>
+    <>
+      <header className="headerTop">
+        <h1 className="titulo">Hola Mundo!</h1>
+      </header>
+      <navbar className="navBar">
+        <div className="enlacesNavBar">
+          <a href="#" className="linkProducto">Link</a>
+          <a href="#" className="linkProducto">Link</a>
+          <a href="#" className="linkProducto">Link</a>
+        </div>
+        <div className="enlacesCarrito">
+          <a href="#" className="iconoCarrito">Carrito</a>
+        </div>
+      </navbar>
+    </>
   )
 }
 
